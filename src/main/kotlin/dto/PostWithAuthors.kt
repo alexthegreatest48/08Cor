a6e22481119c6a4113956a551d888c6879012a2f
alexthegreatest48 <115409767+alexthegreatest48@users.noreply.github.com>
2023-07-30 03:14:10 +0300

    First commit

---
+++ src/main/kotlin/dto/PostWithAuthors.kt
@@ -0,0 +1,6 @@
+package dto
+
+data class PostWithAuthors(
+    val post: Post,
+    val author: Author
+)
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,55 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import dto.Author
+import dto.Post
+import kotlinx.coroutines.*
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+private val client = OkHttpClient.Builder()
+    .connectTimeout(10, TimeUnit.SECONDS)
+    .addInterceptor(HttpLoggingInterceptor().apply{ level = HttpLoggingInterceptor.Level.BODY
+    })
+    .build()
+
+private const val BASE_URL = "http://10.0.2.2:9999/api/slow"
+private val gson = Gson()
+
+suspend fun OkHttpClient.apiCall(url: String): Response{
+    return suspendCoroutine {continuation ->
+        Request.Builder()
+            .url(url)
+            .build()
+            .let(::newCall).enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response)
+                }
+            })
+    }
+}
+
+suspend fun <T> makeRequest(url: String, typeToken: TypeToken<T>): T =
+    withContext(Dispatchers.IO){
+        gson.fromJson(client.apiCall(url).body?.string(), typeToken)
+    }
+
+
+fun main(){
+    CoroutineScope(EmptyCoroutineContext).launch {
+        val posts = makeRequest(url = "$BASE_URL/posts", object : TypeToken<List<Post>>() {})
+        posts.map{post ->
+           async{ makeRequest(url = "$BASE_URL/authors/{$post.id}", object : TypeToken<List<Author>>(){})}
+        }.awaitAll()
+    }
+    Thread.sleep(100000)
+}
